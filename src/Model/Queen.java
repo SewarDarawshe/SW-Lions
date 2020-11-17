ca3dbfd98aaf2102f28a091b1c8ef9685b1d3df5
Alaakh95 <alaakh943@gmail.com>
2020-11-17 18:19:04 +0200

    Update Queen.java

--- src/Model/Queen.java
+++ src/Model/Queen.java
@@ -1,10 +1,37 @@
 package Model;
 
-public class Queen {
+public class Queen extends Soldier {
+	
+	private Soldier queen;
+	
+	// -------------------------------Constructors-------------------------------
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	public Queen(boolean isAlive, boolean isQueen, Square location) {
+		super(isAlive, isQueen, location);
+		// TODO Auto-generated constructor stub
 	}
+	
+	// -------------------------------Getters And Setters-------------------------
+
+	public Soldier getQueen() {
+		return queen;
+	}
+
+	public void setQueen(Soldier queen) {
+		this.queen = queen;
+	}
+
+	@Override
+	public String toString() {
+		return "Queen []";
+	}
+	
+	// -------------------------------Methods------------------------------------
+	
+	//To Do
+	public void main() {
+		
+	}
+	
 
 }
