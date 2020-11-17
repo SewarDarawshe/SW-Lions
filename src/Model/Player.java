c94a7058ff5b973b1d6b7fd21f041452443359c6
Alaakh95 <alaakh943@gmail.com>
2020-11-17 18:13:58 +0200

    Update Player.java

--- src/Model/Player.java
+++ src/Model/Player.java
@@ -1,5 +1,42 @@
 package Model;
 
 public class Player {
+	// -------------------------------Class Members------------------------------
+	
+	private String NickName;
+	private int Points;
+	
+	// -------------------------------Constructors-------------------------------
 
+	public Player(String nickName, int points) {
+		super();
+		this.NickName = nickName;
+		this.Points = points;
+	}
+	
+	// -------------------------------Getters And Setters-------------------------
+
+	public String getNickName() {
+		return NickName;
+	}
+
+	public void setNickName(String nickName) {
+		this.NickName = nickName;
+	}
+
+	public int getPoints() {
+		return Points;
+	}
+
+	public void setPoints(int points) {
+		this.Points = points;
+	}
+
+	@Override
+	public String toString() {
+		return "Player [NickName=" + NickName + ", Points=" + Points + "]";
+	}
+	 
+	
 }
+
