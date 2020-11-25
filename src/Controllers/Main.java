7a22f0296fa4940a77fa4bcb439c2b5b69fcf819
SewarDrawhe <45765243+SewarDrawhe@users.noreply.github.com>
2020-11-26 00:49:23 +0200

    sysdata partly done
    succesfully reading\adding\editing questions from the Json file :)
    2 json jars added

--- src/Controllers/Main.java
+++ src/Controllers/Main.java
@@ -12,18 +12,13 @@ public class Main extends Application {
 	@Override
 	public void start(Stage primaryStage) {
 		try {
-			Parent root=FXMLLoader.load(getClass().getResource("/view/MainBoardScreen.fxml"));
-			Scene scene = new Scene(root);
-			primaryStage.setTitle("Home Page");
+			//Parent root=FXMLLoader.load(getClass().getResource("/view/MainBoardScreen.fxml"));
+			//Scene scene = new Scene(root);
+		//	primaryStage.setTitle("Home Page");
 			//scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
-			primaryStage.setScene(scene);
-			primaryStage.show();
-			for(Question q:Sysdata.getInstance().getQuestionsarr())
-			{
-				System.out.println("hi");
-
-				System.out.println(q);
-			}
+			//primaryStage.setScene(scene);
+		//	primaryStage.show();
+			System.out.println("gonna be okay :)");
 				
 		} catch(Exception e) {
 			e.printStackTrace();
--- src/Controllers/Sysdata.java
+++ src/Controllers/Sysdata.java
@@ -2,10 +2,17 @@ package Controllers;
 
 import java.io.FileNotFoundException;
 
+
+
 import java.io.FileReader;
 import java.io.IOException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
 import java.util.Iterator;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Random;
 
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
@@ -85,7 +92,7 @@ public class Sysdata {
 					}
 				int k = 1;
 
-				Object obj = new JSONParser().parse(new FileReader("questionsJSON.json"));
+				Object obj = new JSONParser().parse(new FileReader("Questions.json"));
 				JSONObject jo = (JSONObject) obj;
 				JSONArray arr = (JSONArray) jo.get("questions");
 
@@ -112,7 +119,6 @@ public class Sysdata {
 			
 				  Question q = new Question(k, context, difficulty, arrlista, correct_ans);
 					k++;
-					System.out.println("aa");
 					QuestionsArray.add(q);
 				}
 			} catch (FileNotFoundException e) {
@@ -124,8 +130,118 @@ public class Sysdata {
 			}
 		}
 
-	
-	
-	
+		/*
+		 * given a question adds it to the data set of questions (the questionarr) and
+		 * to the JSON file by rewriting it iff we don't already have the same question.
+		 */
+		public boolean addQuestion(Question q) {
+			for (Question question : QuestionsArray) {
+				if (question.equals(q)) {
+					System.out.println("we already have this question");
+					return false;
+				}
+				QuestionsArray.add(q);
+				try {
+					writeQuestionsToJSON();
+					return true;
+				} catch (Exception e) {
+					e.printStackTrace();
+					return false;
+				}
+			}
+			return false;
+		}
+		/*
+		 * Given an array list this method overrides the JSON questions file with the
+		 * questions in the array list
+		 */
+		@SuppressWarnings({ "deprecation", "unchecked" })
+		private void writeQuestionsToJSON() {
+			try {
+				JSONObject jo = new JSONObject();
+				JSONArray ja = new JSONArray();
+
+				for (Question q : QuestionsArray) {
+					@SuppressWarnings("rawtypes")
+					Map m = new LinkedHashMap(5);
+					m.put("question", q.getText());
+					List<String> list = new ArrayList<>();
+					for (Answer a : q.getAnswers()) {
+						list.add(a.getText());
+					}
+					JSONArray answersarr = new JSONArray(list);
+					m.put("answers", answersarr);
+					m.put("correct_ans", "" + q.getCorrect_ans());
+					m.put("level", "" + q.getDifficulty().getNumber());
+					m.put("team", q.getTeam());
+					ja.add(m);
+				}
+				jo.put("questions", ja);
+				PrintWriter pw = new PrintWriter("Questions.json");
+				pw.write(jo.toJSONString());
+				pw.flush();
+				pw.close();
+
+			} catch (FileNotFoundException e) {
+				e.printStackTrace();
+			} catch (Exception e) {
+				e.printStackTrace();
+
+			}
+		}
+		/*
+		 * given an old and new Question adds the new one and removes the old.
+		 */
+		public boolean editQuestion(Question old, Question newq) {
+			if (old != null && newq != null) {
+				if (removeQuestion(old))
+					if (addQuestion(newq))
+						return true;
+			}
+			return false;
+		}
+		
+		/*
+		 * given a question removes it from the array list and from the JSON file.
+		 */
+		public boolean removeQuestion(Question q) {
+			if (q != null) {
+				QuestionsArray.remove(QuestionsArray.indexOf(q));
+				writeQuestionsToJSON();
+				readQuestionsJSON();
+				return true;
+			}
+			return false;
+		}
+
+		/*
+		 * given a difficulty returns a random question with the requested difficulty.
+		 */
+		public Question fetchQuestion(E_Difficulty diff) {
+			ArrayList<Question> arr = fetchQuestionsArr(diff);
+			Random rand = new Random();
+			int s = arr.size();
+			if (s > 0) {
+				Question choosenQuestion = arr.get(rand.nextInt(s));
+				return choosenQuestion;
+			} else {
+				System.out.println("there are no questions with difficulty " + diff.toString());
+				return null;
+			}
+		}
+		
+		/*
+		 * given a difficulty returns an array list of all questions with the given
+		 * difficulty.
+		 */
+		public ArrayList<Question> fetchQuestionsArr(E_Difficulty diff) {
+			QuestionsArray = getQuestionsarr();
+			ArrayList<Question> choosenQuestions = new ArrayList<Question>();
+			for (Question q : QuestionsArray) {
+				if (q.getDifficulty().equals(diff))
+					choosenQuestions.add(q);
+			}
+			return choosenQuestions;
+		}
 
 }
