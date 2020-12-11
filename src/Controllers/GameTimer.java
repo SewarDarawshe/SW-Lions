89e6709f2f5c52f8ceac80efc2925f5459ee433d
etharbakir <73297329+etharbakir@users.noreply.github.com>
2020-12-11 13:25:11 +0200

    Revert "update timer pause"
    This reverts commit 4247a816502d0ded7572e6cfc8a719101991f973.

--- src/Controllers/GameTimer.java
+++ src/Controllers/GameTimer.java
@@ -1,140 +1,89 @@
-
-
 package Controllers;
 
 import java.util.Timer;
 import java.util.TimerTask;
 
-import java.util.concurrent.Future;
-
-
 import utils.E_TimerStatus;
-import utils.GameState;
+import utils.player_turn;
 
-/**
- * 
- * A abstract timer utility class, that supports pause and resume.
- * 
- * Can be used either as a normal timer or a countdown timer.
- * 
- * @author ethar bakir
- * 
- */
-public abstract class GameTimer {
+public class GameTimer {
 	
-	
-	public static final int DURATION_INFINITY = -1;
-	private volatile boolean isRunning = false;
-	private long interval;
-	private long elapsedTime;
-	private long duration;
-	//private ScheduledExecutorService execService = Executors
-	//		.newSingleThreadScheduledExecutor();
-	private Future<?> future = null;
 	private E_TimerStatus Status;
+	private Timer timer;
+	private int startTime; // the time the timer started
+	private int time=60;
+	private int timeLeft; // case paused, holds the left for completion
+	private int timePast=0;
 	
-
-	/**
-	 * Default constructor which sets the interval to 1000 ms (1s) and the
-	 * duration to {@link TimerGame#DURATION_INFINITY}
-	 */
-	public GameTimer() {
-		this(1000, -1);
+	
+	
+   
+	
+	
+public GameTimer() {
+		super();
 	}
 
-	/**
-	 * @param interval
-	 *            The time gap between each tick in millis.
-	 * @param duration
-	 *            The period in millis for which the timer should run. Set it to {@code Timer#DURATION_INFINITY} if the timer has to run indefinitely.
-	 */
-	public GameTimer(long interval, long duration) {
-		this.interval = interval;
-		this.duration = duration;
-		this.elapsedTime = 0;
-	}
-
-	/**s
-	 * Starts the timer. If the timer was already running, this call is ignored.
-	 */
-	
-	Timer timer=new Timer();
-    TimerTask timerTask=new TimerTask() {
-		
-		@Override
-		public void run() {
-         elapsedTime++;		
-         System.out.println(elapsedTime);
-		}
-	}; 
-	
+	//take the time that the game started	
 	public void start() {
-		if (isRunning)
-			return;
-
-		isRunning = true;
-		Status= E_TimerStatus.RUNNING;
 		
-	
-		timer.scheduleAtFixedRate(timerTask, 1000, 1000);
+
+		startTime = (int) System.currentTimeMillis() / 1000;
+		Status = E_TimerStatus.RUNNING;
 		
 	}
-
 	
 	/**
-	 * Paused the timer. If the timer is not running, this call is ignored.
+	 * pauses the timer. keeps the time left in timeLeft
 	 */
+	//take the time that the time paused
+	//calculate the time that based from the start until now(paused) in second
+	// and put the time left for the turn into time left
 	public void pause() {
-		if(!isRunning) return;
-		future.cancel(false);
-		isRunning = false;
-		Status= E_TimerStatus.PAUSED;
+		int cancelTime = (int) System.currentTimeMillis() / 1000;
+		int timePast = cancelTime - startTime;
+	    timeLeft = time - timePast;
+
+		Status = E_TimerStatus.PAUSED;
+		
+		if (timer != null)
+			timer.cancel();
+		
 
 	}
-
 	
-	/**
-	 * Resumes the timer if it was paused, else starts the timer.
-	 */
 	public void resume() {
-		this.start();
+		timer = new Timer();
+
+		Status = E_TimerStatus.RUNNING;
+		
 
 	}
-
 	
+	public void pointsSum()
+	{
+		int timePast = time - startTime;
+	    timeLeft = time - timePast;
+	    
+	    
+	     
+	}
 	
 	/**
-	 * Stops the timer. If the timer is not running, then this call does nothing.
+	 * @return gets status of the timer
+	 */
+	public E_TimerStatus getStatus() {
+		return Status;
+	}
+
+	/**
+	 * cancels the timer tasks
 	 */
 	public void cancel() {
-		pause();
-		this.elapsedTime = 0;
-	}
-
-	
-	/**
-	 * @return the elapsed time (in millis) since the start of the timer.
-	 */
-	public long getElapsedTime() {
-		return this.elapsedTime;
+		Status = utils.E_TimerStatus.STOPPED;
+		if (timer != null)
+			timer.cancel();
 	}
 	
-	/**
-	 * @return the time remaining (in millis) for the timer to stop. If the duration was set to {@code Timer#DURATION_INFINITY}, then -1 is returned.
-	 */
-	public long getRemainingTime(){
-		if(this.duration <0){
-			return GameTimer.DURATION_INFINITY;
-		}
-		else{
-			return duration-elapsedTime;
-		}
-	}
-	
-	/**
-	 * @return true if the timer is currently running, and false otherwise.
-	 */
-	public boolean isRunning() {
-		return isRunning;}
 
 }
